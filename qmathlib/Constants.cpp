53e66aeac3ddd1e1f16badc2bbfb2bac316781b7
GodsAperture <32313881+GodsAperture@users.noreply.github.com>
2021-03-30 17:14:21 -0400

    Create Constants.cpp

---
+++ qmathlib/Constants.cpp
@@ -0,0 +1,23 @@
+namespace qmath{
+
+    double QMATHDELTA = 0.1;
+
+    const double Pi = 3.1415926535897932;
+
+    const double E  = 2.7182818284590452;
+
+    const double Gold = 1.6180339887498948;
+
+}
+
+namespace qmathf{
+
+    float QMATHDELTA = 0.1;
+
+    const float Pi = 3.14159265;
+
+    const float E  = 2.71828182;
+
+    const float Gold = 1.61803398;
+
+}
